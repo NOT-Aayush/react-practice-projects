45023f9fb2e192f33f7bb592d9cef5d08bd8e569
NOT-Aayush <aayush4373pandey@gmail.com>
2026-03-28 21:35:52 +0530

    api integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,29 @@
 import Moviecard from "../components/Moviecard"
-import {useState} from "react"
+import {useState ,useEffect} from "react"
+import { SearchMoives ,getPopularMoives } from "../services/api";
 import "../css/Home.css"
 
 function Home(){
     const [searchQuery, setSearchQuery ] = useState("");
-    let movies =[
-        {id:1, title:"John Wick", release_date:"2022"},
-        {id:2, title:"Terminator", release_date:"1998"},
-        {id:3, title:"Rocky", release_date:"1986"},
-        {id:4, title:"matrix", release_date:"2002"}
-    ]
+    const [movies ,setMovies ] = userState([]);
+    const [error, setError] = useState(null);
+    const [loading , setLoading] = useStare(true)
+
+    useEffect(()=> {
+        const loadpopularMovies = async () =>{
+            try{
+                const popularMovies =await getPopularMoives()
+                setMovies(popularMovies)
+            }catch (err){
+                console.log(err)
+                setError("Falied to load movies")
+            }
+            finally{
+                setLoading(false)
+            }
+        }
+        loadpopularMovies()
+    }, [SearchMoives ,getPopularMoives])
 
     const handelSearch = (e) => {
         e.preventDefault()
--- src/services/api.js
+++
@@ -1,6 +0,0 @@
-const API_KEY = "523b110a7b73ebcfed3358d048435a83"
-const BASE_URL = "https://api.themoviedb.org/3"
-
-export const getPopularMoives = async () => {
-    
-}
