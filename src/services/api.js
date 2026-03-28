fb7ac712692776d1d92f0c8b5795aeaed6985b11
NOT-Aayush <aayush4373pandey@gmail.com>
2026-03-28 21:37:48 +0530

    api integration

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const API_KEY = "x"
-const BASE_URL = "x"
+const API_KEY = "523b110a7b73ebcfed3358d048435a83"
+const BASE_URL = "https://api.themoviedb.org/3"
 
 export const getPopularMoives = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
